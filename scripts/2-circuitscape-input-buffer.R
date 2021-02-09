cb42dc34770266fcf532ed0b9ffba02e8d99c211
bronwynrayfield <bronwyn.rayfield@apexrms.com>
2021-02-09 10:25:52 -0500

    Update 2-circuitscape-input-buffer.R

--- scripts/2-circuitscape-input-buffer.R
+++ scripts/2-circuitscape-input-buffer.R
@@ -1,33 +1,46 @@
 # 235
-# Create an analysis buffer for California to remove edge effect during Circuitscape analyses
-
+# Create an analysis buffer for California to remove edge effect during Circuitscape connectivity analyses
 
 library(raster)
-library(sf)
 library(tidyverse)
 
 # working dir is assumed to be the a235 github repo
 dataDir <- "data"
 
-# Read in states
+# Read in initial LULC for CONUS
+sc <- raster(file.path("../", "national-assessment", "data", "initial-conditions", "final", "ic-state-class.tif"))
+
+# Read in states and create binary study area
 icStates <- raster(file.path(dataDir, "initial-conditions", "final", "ic-states.tif"))
-
-# Convert raster to polygon
-studyArea <- rasterToPolygons(icStates)
-
+studyArea <- Which(icStates>0)
+studyArea[studyArea==0] <- NA
 
 # Create bounding box around study area
 # Calculate 20% of the average of the max length and width of the bounding box to be the study area buffer width
 # Connectivity analyses will be run in this buffered study area to minimize edge effects
-studyAreaBBox <- st_bbox(studyArea)
-studyAreaBufferWidth <- round(((studyAreaBBox$ymax - studyAreaBBox$ymin) + (studyAreaBBox$xmax - studyAreaBBox$xmin)) / 2 * 0.2, digits = 0) # in m
-studyAreaBuffer <- st_buffer(studyArea, studyAreaBufferWidth)
+studyAreaBufferWidth <- round(((ymax(icStates) - ymin(icStates)) + (xmax(icStates) - xmin(icStates))) / 2 * 0.2, digits = 0) # in m
+studyArea <- extend(studyArea, extent(xmin(studyArea) - studyAreaBufferWidth, 
+                                      xmax(studyArea) + studyAreaBufferWidth, 
+                                      ymin(studyArea) - studyAreaBufferWidth, 
+                                      ymax(studyArea) + studyAreaBufferWidth))
+studyAreaBuffer <- buffer(studyArea, studyAreaBufferWidth, doEdge=T)
 
-## Crop LULC data to STUDY AREA BUFFER region and standardize rasters ---------------------
 
-# SOLRIS data
-# Crop to buffered study area
-SOLRIS_buffer <- SOLRISRaw %>%
-  crop(., extent(studyAreaBuffer), snap="out") %>% # Crop SOLRIS to study area buffer extent
-  mask(., mask=studyAreaBuffer) %>% # Clip to buffered study area
-  trim(.) # Trim extra white spaces
+# Create a raster with values of open water (value = 11)
+waterBuffer <- studyAreaBuffer
+values(waterBuffer) <- 11
+waterBuffer <- mask(waterBuffer, studyAreaBuffer)
+
+## Crop LULC data to study area buffer region
+scCrop <- sc %>%
+  crop(., studyAreaBuffer) %>%
+  extend(., studyAreaBuffer) %>% #Note: need to extend original CONUS extent into water
+  mask(., studyAreaBuffer)
+
+# Merge CONUS and water buffers  
+scBuffered <- merge(scCrop, waterBuffer)
+
+# Write raster
+writeRaster(scCrop, file.path(dataDri, "circuitscape", "sc-buffered.tif"))
+writeRaster(scBuffered, file.path(dataDri, "circuitscape", "sc-buffered-with-water.tif"))
+writeRaster(waterBuffer, file.path(dataDri, "circuitscape", "water-buffer.tif"))
